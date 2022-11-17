b6749a5de3bf4414eae2077533f15e1acccdcd35
Damir Rashidinov <damir.dmr88@gmail.com>
2022-11-17 11:53:43 +0600

    media 100 upd gallery img

--- mail.php
+++ mail.php
@@ -1,45 +1,35 @@
 <?php
-
 $method = $_SERVER['REQUEST_METHOD'];
-
-//Script Foreach
-$c = true;
 if ( $method === 'POST' ) {
-
-	$project_name = trim($_POST["project_name"]);
-	// $admin_email  = trim($_POST["admin_email"]);
+	$frm_name  = "Регистрация на Розыгрыш автомобиля";
+	$recepient = "no-reply@happyhome.kz";
+	$sitename  = "Happy Home";
+	$subject   = "Регистрация на Розыгрыш автомобиля с сайта \"$sitename\"";
 	$admin_email  = 'marketing@happyhome.kz';
-	$form_subject = trim($_POST["form_subject"]);
 
-	foreach ( $_POST as $key => $value ) {
-		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
-			$message .= "
-			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-			</tr>
-			";
-		}
-	}
-} else if ( $method === 'GET' ) {
+	$name = trim($_POST["name"]);
+	$sname = trim($_POST["sname"]);
+	$phone = trim($_POST["phone"]);
 
-	$project_name = trim($_GET["project_name"]);
-	// $admin_email  = trim($_GET["admin_email"]);
-	$admin_email  = 'marketing@happyhome.kz';
-	$form_subject = trim($_GET["form_subject"]);
+	$message = "
+	<tr>
+		<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>ФИО</b></td>
+		<td style='padding: 10px; border: #e9e9e9 1px solid;'>$name</td>
+	</tr>
+	<tr>
+		<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>Фамилия</b></td>
+		<td style='padding: 10px; border: #e9e9e9 1px solid;'>$sname</td>
+	</tr>
+	<tr>
+		<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>Телефон</b></td>
+		<td style='padding: 10px; border: #e9e9e9 1px solid;'>$phone</td>
+	</tr>
+	<tr>
+		";
 
-	foreach ( $_GET as $key => $value ) {
-		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
-			$message .= "
-			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-			</tr>
-			";
-		}
-	}
+	$message = "<table style='width: 100%;'>$message</table>";
+
+	if(!mail($admin_email, $subject, $message, "From: $recepient" . "\r\n" . "Reply-To: $recepient" . "\r\n" . "X-Mailer: PHP/" . phpversion() . "\r\n" . "Content-type: text/html; charset=\"utf-8\"")){echo ":(";};
+} else {
+	echo "Ошибка при отправке формы, пожалуйста повторите";
 }
-
-$message = "<table style='width: 100%;'>$message</table>";
-
-mail($admin_email, $form_subject, $message, "From: $project_name <$admin_email>" . "\r\n" . "Reply-To: $admin_email" . "\r\n" . "X-Mailer: PHP/" . phpversion() . "\r\n" . "Content-type: text/html; charset=\"utf-8\"");
